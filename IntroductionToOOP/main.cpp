7178cab6f601c690bf875594d1b392434ef90954
Max-Degtyarev <Usamaximum@mail.ru>
2022-05-11 22:20:04 +0300

    OOP

--- IntroductionToOOP/main.cpp
+++ IntroductionToOOP/main.cpp
@@ -1,16 +1,36 @@
 ﻿#include<iostream>
 using namespace std;
 
-struct Point
+class Point
 {
 	double x;
 	double y;
+public:
+	double get_x()const
+	{
+		return x;
+	}
+	double get_y()const
+	{
+		return y;
+	}
+	void set_x(double x)
+	{
+		this->x = x;
+	}
+	void set_y(double y)
+	{
+		this->y = y;
+	}
 
 };
+
+//#define STRUCT_POINT
 
 void main()
 {
 	setlocale(LC_ALL, "");
+#ifdef STRUCT_POINT
 	int a; //Объявление переменной 'a' типа 'int'
 	Point A; //Объявление переменной 'A' типа 'Point'
 			// Объявление объекта 'A' структуры 'Point'
@@ -24,6 +44,19 @@ void main()
 
 	cout << sizeof(Point) << endl;
 	cout << typeid(A).name() << endl;
+#endif
+
+	Point A;
+	double x = 2, y = 3;
+	//cout << "Введите координаты точки :"; cin >> x >> y;
+	A.set_x(x);
+	A.set_y(y);
+	cout << A.get_x() << "\t" << A.get_y() << endl;
+	Point B;
+	B.set_x(7);
+	B.set_y(8);
+	cout << B.get_x() << "\t" << B.get_y() << endl;
+
 
 
 
