eebd85e11ce462f94307c634283316b59ae72d0c
Max-Degtyarev <Usamaximum@mail.ru>
2022-05-11 20:50:13 +0300

    Add project files.

---
+++ IntroductionToOOP/main.cpp
@@ -0,0 +1,30 @@
+﻿#include<iostream>
+using namespace std;
+
+struct Point
+{
+	double x;
+	double y;
+
+};
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	int a; //Объявление переменной 'a' типа 'int'
+	Point A; //Объявление переменной 'A' типа 'Point'
+			// Объявление объекта 'A' структуры 'Point'
+	A.x = 2;
+	A.y = 3;
+	cout << A.x << "\t" << A.y << endl;
+
+	Point* pA = &A;
+	cout << pA->x << "\t" << pA->y << endl;
+	cout << (*pA).x << "\t" << (*pA).y << endl;
+
+	cout << sizeof(Point) << endl;
+	cout << typeid(A).name() << endl;
+
+
+
+}
